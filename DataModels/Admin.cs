6f49a316ee7a129b8d54da57eafcab2b6139f8b9
choxkri <1059788@hr.nl>
2023-03-22 14:07:47 +0100

    Admin class toegevoegd

---
+++ DataModels/Admin.cs
@@ -0,0 +1,22 @@
+﻿public class Admin
+{
+    public string UserName;
+    public string Password;
+    public bool IsLoggedIn;
+
+    public Admin(string u, string p)
+    {
+        this.UserName = u;
+        this.Password = p;
+    }
+    
+    public void LogIn(string u, string p) 
+    {
+        this.IsLoggedIn = true;
+    }
+
+    public void LogOut()
+    {
+        this.IsLoggedIn = false;
+    }
+}
