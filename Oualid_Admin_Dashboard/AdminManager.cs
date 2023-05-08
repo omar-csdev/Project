402bfb4b69bdc3e324196ee235ddf043ccfbd665
choxkri <1059788@hr.nl>
2023-05-08 11:13:29 +0200

    admin can view other admin accounts

--- Oualid_Admin_Dashboard/AdminManager.cs
+++ Oualid_Admin_Dashboard/AdminManager.cs
@@ -2,11 +2,29 @@
 {
     public static void Start()
     {
-        
+        Console.Clear();
+        ShowAccounts();
+        Console.WriteLine("Press enter to go back.");
+        var input = Console.ReadLine();
+        AdminDashboard.DisplayDashboard();
     }
 
     public static void ShowAccounts()
     {
         List<Admin> AllAccounts = LoginAccess.LoadAll("admindata.json");
+        int i = 0;
+        foreach (Admin admin in AllAccounts) 
+        {
+            string x = "Offline";
+            if (admin.IsLoggedIn) 
+            {
+                x = "Online";
+            }
+            if (i != 0)
+            {
+                Console.WriteLine($"{i} | {admin.UserName} | {x}");
+            }
+            i += 1;
+        }
     }
 }
