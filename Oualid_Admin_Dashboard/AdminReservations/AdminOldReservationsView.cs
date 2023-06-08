5afc3851f4f52f64bc9881098d5f697fc9fef24b
OlivierS4242 <112190173+OlivierS4242@users.noreply.github.com>
2023-06-08 14:27:02 +0200

    View old reservations and customer ID in reservations

--- Oualid_Admin_Dashboard/AdminReservations/AdminOldReservationsView.cs
+++ Oualid_Admin_Dashboard/AdminReservations/AdminOldReservationsView.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 static class AdminOldReservationsView
 {
     public static List<Project.Olivier_Reservations.Reservation> reservations = Project.Olivier_Reservations.SaveReservations.LoadAll();
+    private static Dictionary<int, string> filePaths = new Dictionary<int, string>()
+    {
+        { 2, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1YearAgo.json" },
+        { 3, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1To2YearsAgo.json" },
+        { 4, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin2To3YearsAgo.json" },
+        { 5, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1To2YearsAgo.json" },
+        { 6, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin2To3YearsAgo.json" },
+        { 7, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1To2YearsAgo.json" },
+    };
 
     public static void ViewOldReservations()
     {
@@ -29,7 +37,7 @@ static class AdminOldReservationsView
                 if (choice < 1 || choice > 7)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Enter a number between 1 and 4.");
+                    Console.WriteLine($"Enter a number between 1 and 7.");
                     Console.ResetColor();
                     continue;
                 }
@@ -38,7 +46,7 @@ static class AdminOldReservationsView
             catch (FormatException)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Please enter a valid number between 1 and 4.");
+                Console.WriteLine("Please enter a valid number between 1 and 7.");
                 Console.ResetColor();
             }
             catch (Exception ex)
@@ -49,18 +57,53 @@ static class AdminOldReservationsView
             }
         }
 
-        switch (choice)
+        if (choice == 1)
         {
-            case 1:
-                string filePath2 = Path.Combine(Environment.CurrentDirectory, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1YearAgo.json");
+            // View all old reservations
+            var allReservations = GetAllOldReservations();
+            foreach (var reservation in allReservations)
+            {
+                PrintReservation(reservation);
+            }
+        }
+        else if (filePaths.ContainsKey(choice))
+        {
+            string filePath = Path.Combine(Environment.CurrentDirectory, filePaths[choice]);
+            string jsonString = File.ReadAllText(filePath);
+            List<Reservation> existingReservations = JsonConvert.DeserializeObject<List<Reservation>>(jsonString) ?? new List<Reservation>();
 
-                break;
-
+            foreach (var reservation in existingReservations)
+            {
+                PrintReservation(reservation);
+            }
         }
 
+        Console.WriteLine("Press any key to go back...");
+        Console.ReadKey();
     }
 
-   
+    private static List<Reservation> GetAllOldReservations()
+    {
+        var allReservations = new List<Reservation>();
 
+        foreach (var filePath in filePaths.Values)
+        {
+            string fullPath = Path.Combine(Environment.CurrentDirectory, filePath);
+            string jsonString = File.ReadAllText(fullPath);
+            List<Reservation> reservations = JsonConvert.DeserializeObject<List<Reservation>>(jsonString) ?? new List<Reservation>();
+            allReservations.AddRange(reservations);
+        }
+
+        return allReservations;
+    }
+
+    private static void PrintReservation(Reservation reservation)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Name: " + reservation.Name + " " + reservation.LastName);
+        Console.WriteLine("Group size: " + reservation.GroupSize);
+        Console.WriteLine("Reservation date and time: *NOT WORKING?*");
+        Console.WriteLine("Reservation Code: " + reservation.Code);
+        Console.WriteLine("");
+    }
 }
-
