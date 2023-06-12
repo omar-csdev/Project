85c7fb8e241e639e03d5ffc7aeb4ddca09222ed2
Elfarouq <1063106@hr.nl>
2023-06-12 15:24:29 +0200

    Update RevenueMade.cs
    Made it possible to see the revenue per in 7 different rows.

--- Elfarouq_FoodMenu/RevenueMade.cs
+++ Elfarouq_FoodMenu/RevenueMade.cs
@@ -9,11 +9,11 @@ using System.Text;
 using System.Threading.Tasks;
 using ScottPlot;
 
+
 public class RevenueMade
 {
     public static void GenerateRevenue()
     {
-        Console.WriteLine("dawdawdad");
         string filePath = Path.Combine(Environment.CurrentDirectory, @"..\..\..\DataSources\PaidOrders.json");
 
         string jsonString = File.ReadAllText(filePath);
@@ -47,6 +47,78 @@ public class RevenueMade
 
         // Printing the Table with the revenues
         table.Write();
+        RevenuePerYear();
         Console.ReadLine();
     }
+    public static void RevenuePerYear()
+    {
+        //voeg misschien nog amount of visitors toe.
+        string filePath = Path.Combine(Environment.CurrentDirectory, @"..\..\..\DataSources\PaidOrders.json");
+
+        string jsonString = File.ReadAllText(filePath);
+        double yearNow = 0;
+        double oneYearAgo = 0;
+        double twoYearAgo = 0;
+        double threeYearAgo = 0;
+        double fourYearAgo = 0;
+        double fiveYearAgo = 0;
+        double aboveFiveYearAgo = 0;
+        DateTime dateNow = DateTime.Now;
+        DateTime date1 = dateNow.AddYears(-1);
+        DateTime date2 = dateNow.AddYears(-2);
+        DateTime date3 = dateNow.AddYears(-3);
+        DateTime date4 = dateNow.AddYears(-4);
+        DateTime date5 = dateNow.AddYears(-5);
+        DateTime date5plus = dateNow.AddYears(-6);
+
+        List<PaidOrder> existingData = JsonConvert.DeserializeObject<List<PaidOrder>>(jsonString) ?? new List<PaidOrder>();
+        Dictionary<DateTime, Double> RevenuePerYear = new Dictionary<DateTime, double>();
+        List<double> YearlyRevenue = new List<double>();
+        foreach(PaidOrder order in existingData) 
+        {
+           if (order == null) continue;
+            if (order.PaidTime.Year == dateNow.Year) 
+            {
+                yearNow += order.TotalPrice;
+                continue;
+            }
+            if (order.PaidTime.Year == date1.Year)
+            {
+                oneYearAgo += order.TotalPrice;
+                continue;
+            }
+            if (order.PaidTime.Year == date2.Year)
+            {
+                twoYearAgo += order.TotalPrice;
+                continue;
+            }
+            if (order.PaidTime.Year == date3.Year)
+            {
+                threeYearAgo += order.TotalPrice;
+                continue;
+            }
+            if (order.PaidTime.Year == date4.Year)
+            {
+                fourYearAgo += order.TotalPrice;
+                continue;
+            }
+            if (order.PaidTime.Year == date5plus.Year)
+            {
+                aboveFiveYearAgo += order.TotalPrice;
+                continue;
+            }
+            else { continue; }
+            
+        }
+        ConsoleTable table = new ConsoleTable("Year", "RevenueMade");
+        table.AddRow(dateNow.Year, yearNow);
+        table.AddRow(date1.Year, oneYearAgo);
+        table.AddRow(date2.Year, twoYearAgo);
+        table.AddRow(date3.Year, threeYearAgo);
+        table.AddRow(date4.Year, fourYearAgo);
+        table.AddRow(date5.Year, fiveYearAgo);
+        table.AddRow(date5plus.Year, aboveFiveYearAgo);
+        table.Write();
+    }
+
 }
