09c10481587e7899a6cdcf0210d936730ab7b17a
Oualid <yabonees@gmail.com>
2023-06-12 21:49:35 +0200

    Customer Dashboard geheel af

--- Oualid_Customer_Dashboard/CustomerDashboard.cs
+++ Oualid_Customer_Dashboard/CustomerDashboard.cs
@@ -8,6 +8,18 @@ public static class CustomerDashboard
     
     private static CustomerAccount customer { get; set; }    
     private static List<Project.Olivier_Reservations.Reservation> upcomingReservations { get; set; }
+  
+    private static Dictionary<int, string> filePaths = new Dictionary<int, string>()
+    {
+        { 2, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1YearAgo.json" },
+        { 3, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1To2YearsAgo.json" },
+        { 4, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin2To3YearsAgo.json" },
+        { 5, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1To2YearsAgo.json" },
+        { 6, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin2To3YearsAgo.json" },
+        { 7, @"..\..\..\DataSources\Oldreservations\oldReservationsWithin1To2YearsAgo.json" },
+    };
+
+
     public static void DisplayDashboard()
     {
         GetLoggedInCustomer();
@@ -28,13 +40,33 @@ public static class CustomerDashboard
                     Project.Olivier_Reservations.Reservations.Reservationstart();
                     break;
                 case "2":
+                    Console.Clear();
+                    WriteLogo(@"
+  _   _                           _                                                  _   _                 
+ | | | |_ __   ___ ___  _ __ ___ (_)_ __   __ _   _ __ ___  ___  ___ _ ____   ____ _| |_(_) ___  _ __  ___ 
+ | | | | '_ \ / __/ _ \| '_ ` _ \| | '_ \ / _` | | '__/ _ \/ __|/ _ \ '__\ \ / / _` | __| |/ _ \| '_ \/ __|
+ | |_| | |_) | (_| (_) | | | | | | | | | | (_| | | | |  __/\__ \  __/ |   \ V / (_| | |_| | (_) | | | \__ \
+  \___/| .__/ \___\___/|_| |_| |_|_|_| |_|\__, | |_|  \___||___/\___|_|    \_/ \__,_|\__|_|\___/|_| |_|___/
+       |_|                                |___/                                                            
+");
                     DisplayReservations(upcomingReservations);
+                    Helper.ContinueDisplay();
                     break;
                 case "3":
-                    ClearScreen();
-                    Console.WriteLine("This feature is not implemented yet!", Color.Green);
+                    List<Project.Olivier_Reservations.Reservation> oldReservations = GetAllOldReservations();
+                    Console.Clear();
+                    WriteLogo(@"
+     _    _ _                                      _   _                 
+    / \  | | |  _ __ ___  ___  ___ _ ____   ____ _| |_(_) ___  _ __  ___ 
+   / _ \ | | | | '__/ _ \/ __|/ _ \ '__\ \ / / _` | __| |/ _ \| '_ \/ __|
+  / ___ \| | | | | |  __/\__ \  __/ |   \ V / (_| | |_| | (_) | | | \__ \
+ /_/   \_\_|_| |_|  \___||___/\___|_|    \_/ \__,_|\__|_|\___/|_| |_|___/
+");
+                    Console.WriteLine("Past reservations: ");
+                    DisplayReservations(oldReservations);
+                    Console.WriteLine("Upcoming reservations: ");
+                    DisplayReservations(upcomingReservations);
                     Helper.ContinueDisplay();
-                    CustomerDashboard.DisplayDashboard();
                     break;
                 case "4":
                     CustomerProfileEditor.DisplayDashboard();
@@ -58,9 +90,9 @@ public static class CustomerDashboard
             Console.Write(prefix, Color.Red);
             Console.WriteLine("] " + message);
         }
-        public static void WriteLogo()
+        public static void WriteLogo(string logoInput = "")
         {
-            string logo = @"
+            string logo = logoInput.Count() > 0 ? logoInput :  @"
   ____             __ _ _      
  |  _ \ _ __ ___  / _(_) | ___ 
  | |_) | '__/ _ \| |_| | |/ _ \
@@ -94,26 +126,21 @@ public static class CustomerDashboard
         public static void GetReservationsForCustomer()
         {
             List<Project.Olivier_Reservations.Reservation> allreservations = Project.Olivier_Reservations.SaveReservations.LoadAll();
-            upcomingReservations = allreservations.Where(r => r.Name == customer.UserNameSetter).ToList();
+            upcomingReservations = allreservations.Where(r => r.CustomerId == customer.ID).ToList();
         }
         public static void DisplayReservations(List<Project.Olivier_Reservations.Reservation> reservations)
         {
             if(reservations.Count == 0) {
-                ClearScreen();
                 Console.WriteLine("No upcoming reservations!");
-                Helper.ContinueDisplay();
                 CustomerDashboard.DisplayDashboard();
             }
             else
             {
-                ClearScreen();
                 foreach (Project.Olivier_Reservations.Reservation reservation in reservations)
                 {
                     AdminReservationsView.DisplayReservation(reservation);
                 }
-                Console.WriteLine();
-                Helper.ContinueDisplay();
-        }   
+            }   
 
         }
         public static void LogUserOut()
@@ -133,5 +160,21 @@ public static class CustomerDashboard
                 AccountManager.LogIn();
             }
         }
-     
+
+        private static List<Project.Olivier_Reservations.Reservation> GetAllOldReservations()
+        {
+            var allReservations = new List<Project.Olivier_Reservations.Reservation>();
+
+            foreach (var filePath in filePaths.Values)
+            {
+                string fullPath = Path.Combine(Environment.CurrentDirectory, filePath);
+                string jsonString = File.ReadAllText(fullPath);
+                List<Project.Olivier_Reservations.Reservation> reservations = JsonConvert.DeserializeObject<List<Project.Olivier_Reservations.Reservation>>(jsonString) ?? new List<Project.Olivier_Reservations.Reservation>();
+                allReservations.AddRange(reservations.Where(r => r.CustomerId == customer.ID));
+        }
+
+            return allReservations;
+        }
+
+
 }
